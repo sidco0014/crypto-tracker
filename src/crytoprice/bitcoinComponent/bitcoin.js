b431e04b809186f11c17badac3ccd6fafce7c69f
sidco0014 <pasari.s@husky.neu.edu>
2018-03-21 22:31:23 -0400

    added new api for bitcoin prices and chart done for bitcoin

--- src/crytoprice/bitcoinComponent/bitcoin.js
+++ src/crytoprice/bitcoinComponent/bitcoin.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import './bitcoin.css';
+import "react-bootstrap/dist/react-bootstrap.min.js";
+// import {Grid, Row, Col} from "react-bootstrap";
+// import NumberFormat from 'react-number-format';
+import {Line} from 'react-chartjs-2';
 
+let bitcoinPrice = [];
+let bitcoinDates = [];
 
 class Bitcoin extends Component {
 
@@ -9,32 +15,83 @@ class Bitcoin extends Component {
         super(props);
 
         this.state = {
-            cryptos: []
+            cryptos: [],
+
+            chartData: {
+                labels: [],
+                datasets: [
+                    {
+                        data: [],
+                    }
+                ],
+            }
         }
     }
 
-
     componentDidMount() {
-        axios.get('https://min-api.cryptocompare.com/data/generateAvg?fsym=BTC&tsym=USD&e=Kraken').then(
+        axios.get('https://api.coindesk.com/v1/bpi/historical/close.json').then(
             res => {
-                const crypto = res.data['RAW'];
+                const crypto = res.data['bpi'];
+                Object.entries(crypto).map((res) => {
+                    bitcoinPrice.push(res[1]);
+                    bitcoinDates.push(res[0]);
+                });
+
                 this.setState({cryptos: crypto});
-            })
+                const charData = {
+                    labels: bitcoinDates,
+                    datasets: [
+                        {
+                            data: bitcoinPrice,
+                            backgroundColor: '#00ea9c',
+                            borderColor: '#fff',
+                            fill: false,
+                            lineTension: 0,
+                            pointRadius: 3,
+                        }
+                    ],
+                };
+
+                this.setState({
+                    chartData: charData
+                })
+            });
     }
 
     render() {
         return (
-            <div className='BitcoinWrapper' id='myScrollToElement'>
+            <div className='BitcoinWrapper'>
                 <h1 className='BitcoinTitleText'>Bitcoin</h1>
-                <p>{this.state.cryptos.PRICE}</p>
-                <p>{this.state.cryptos.FROMSYMBOL}</p>
-                <p>{this.state.cryptos.HIGH24HOUR}</p>
-                <p>{this.state.cryptos.LOW24HOUR}</p>
-                <p>{this.state.cryptos.OPEN24HOUR}</p>
+                <div className='InnerWrapperContent'>
+                    <Line data={this.state.chartData} width={100} height={50} options={{
+                        legend: {
+                            display: false,
+                        },
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    fontColor: '#fff'
+                                },
+                                gridLines: {
+                                    display: false
+                                }
+                            }],
+                            xAxes: [{
+                                ticks: {
+                                    fontColor: '#fff'
+                                },
+                                gridLines: {
+                                    display: false
+                                }
+                            }]
+                        },
+                    }}/>
+                </div>
             </div>
         )
     }
 
 }
+
 
 export default Bitcoin;
